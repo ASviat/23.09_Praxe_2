00429cf75da904d1f6ef14989c8561254bd29525
Sviatoslav <ecrprague.asi@gmail.com>
2022-09-23 18:00:02 +0300

    Task 16 Done

---
+++ Task16/Program.cs
@@ -0,0 +1,14 @@
+﻿bool isSquareNumbers()
+{
+    Console.WriteLine("Введите первое число: ");
+    int n1=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число: ");
+    int n2=Convert.ToInt32(Console.ReadLine());
+
+    return (n1%n2==0 || n2%n1==0);
+}
+
+if (isSquareNumbers()) Console.WriteLine("Да");
+else Console.WriteLine("Нэт");
+
+
